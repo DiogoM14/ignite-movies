ebb6bb96938fb5aede9905e9ba0f5e4214130c3b
DiogoM14 <diogomartins200214@gmail.com>
2021-03-17 18:50:57 +0000

    Finishing Challenge

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
 
 import './styles/global.scss';
+import { MoviesProvider } from './hooks/useMovies';
+
+
+
 
 export function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <SideBar />
-      <Content />
+      <MoviesProvider>
+        <SideBar />
+        <Content />
+      </MoviesProvider>
     </div>
   )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,40 +1,26 @@
-import { useEffect, useState } from "react";
-import { api } from "../services/api";
-import { Button } from "./Button";
+import { useMovies } from '../hooks/useMovies';
+import { Button } from './Button';
 import '../styles/sidebar.scss';
 
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
-
 export function SideBar() {
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-  
-  useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
+  const { genres, handleClickButton, selectedGenreId } = useMovies()
 
   return (
-    <div>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
 
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              id={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-      </nav>
-    </div>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
   )
 }
